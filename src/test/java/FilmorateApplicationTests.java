08a0ca4dd0e4a4501555e9657b9387958b054dbe
Stas <stas23102@yandex.ru>
2023-05-04 03:06:07 +0300

    fix: добавил аннотацию @ContextConfiguration

--- src/test/java/FilmorateApplicationTests.java
+++ src/test/java/FilmorateApplicationTests.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration
 @SpringBootTest
 class FilmorateApplicationTests {
 
